c6409e9d7bd75b574decb8874b8589731c86ed75
lucasrmoro <lucas.rmoro1@gmail.com>
2021-08-20 20:04:35 -0300

    Implementing View binding feature

--- app/src/main/java/br/com/lucas/santanderbootcamp/todolist/MainActivity.kt
+++ app/src/main/java/br/com/lucas/santanderbootcamp/todolist/MainActivity.kt
@@ -2,10 +2,14 @@ package br.com.lucas.santanderbootcamp.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.lucas.santanderbootcamp.todolist.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
