b4ef0936a00b14423d990beff51e387d4de1d8bf
Lucas Moro <lucas.rmoro1@gmail.com>
2022-09-21 20:11:37 -0300

    Creating the AppModule

--- app/src/main/java/br/com/lucas/todo/ui/base/DummyViewModel.kt
+++ app/src/main/java/br/com/lucas/todo/ui/base/DummyViewModel.kt
@@ -3,7 +3,6 @@ package br.com.lucas.todo.ui.base
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @HiltViewModel
 class DummyViewModel @Inject constructor() : ViewModel()
