095311bd911d4a47c5de4652f854f1f41f4a54d7
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-02 15:09:56 -0300

    Adding methods to show/hide menu items

--- app/src/main/java/br/com/lucas/todo/presentation/components/GenericToolbarMenu.kt
+++ app/src/main/java/br/com/lucas/todo/presentation/components/GenericToolbarMenu.kt
@@ -3,12 +3,13 @@ package br.com.lucas.todo.presentation.components
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.annotation.IdRes
 import androidx.annotation.MenuRes
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import br.com.lucas.todo.MainActivity
 
-class GenericToolbarMenu(private val activity: AppCompatActivity) {
+class GenericToolbarMenu(private val activity: MainActivity) {
 
     private var menuProvider: MenuProvider? = null
 
@@ -21,8 +22,17 @@ class GenericToolbarMenu(private val activity: AppCompatActivity) {
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return onMenuItemClicked(menuItem)
             }
+
         }
         menuProvider?.let { (activity as MenuHost).addMenuProvider(it) }
+    }
+
+    fun showMenuItem(@IdRes menuItemId: Int) {
+        activity.showMenuItem(menuItemId)
+    }
+
+    fun hideMenuItem(@IdRes menuItemId: Int) {
+        activity.hideMenuItem(menuItemId)
     }
 
     fun destroy() {
