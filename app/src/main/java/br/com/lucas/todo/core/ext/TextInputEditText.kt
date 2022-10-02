920f5a5d27cdb2469d50c5c59198afdca44b1e99
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-02 15:11:47 -0300

    Extension function to get hour and minute

--- app/src/main/java/br/com/lucas/todo/core/ext/TextInputEditText.kt
+++ app/src/main/java/br/com/lucas/todo/core/ext/TextInputEditText.kt
@@ -2,4 +2,8 @@ package br.com.lucas.todo.core.ext
 
 import com.google.android.material.textfield.TextInputEditText
 
-fun TextInputEditText.getStringText() = text.toString()
+fun TextInputEditText.getStringText() = text?.trim().toString()
+
+fun TextInputEditText.getHour() = text?.split(':')?.get(0) ?: ""
+
+fun TextInputEditText.getMinute() = text?.split(':')?.get(1) ?: ""
