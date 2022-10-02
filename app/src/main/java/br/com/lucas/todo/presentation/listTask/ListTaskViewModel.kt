6c3de63b2d513fc3fe9ac891063201865486f88d
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-02 15:13:20 -0300

    Modifying the list task viewmodel

--- app/src/main/java/br/com/lucas/todo/presentation/listTask/ListTaskViewModel.kt
+++ app/src/main/java/br/com/lucas/todo/presentation/listTask/ListTaskViewModel.kt
@@ -1,39 +1,61 @@
 package br.com.lucas.todo.presentation.listTask
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.lucas.todo.R
 import br.com.lucas.todo.core.ext.viewModelCall
 import br.com.lucas.todo.domain.model.Task
-import br.com.lucas.todo.domain.useCases.DeleteTaskUseCase
+import br.com.lucas.todo.domain.useCases.DeleteSelectedTasksUseCase
 import br.com.lucas.todo.domain.useCases.GetAllTasksUseCase
+import br.com.lucas.todo.domain.useCases.UpdateTaskUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ListTaskViewModel @Inject constructor(
-    private val deleteTaskUseCase: DeleteTaskUseCase,
-    private val getAllTasksUseCase: GetAllTasksUseCase
+    private val updateTaskUseCase: UpdateTaskUseCase,
+    private val getAllTasksUseCase: GetAllTasksUseCase,
+    private val deleteSelectedTasksUseCase: DeleteSelectedTasksUseCase
 ) : ViewModel() {
 
-    val taskList = MutableLiveData<List<Task>>()
+    private var _taskList = MutableLiveData<List<Task>>()
+    val taskList: LiveData<List<Task>>
+        get() = _taskList
+
+    private var _hasSelectedTasks = MutableLiveData<Boolean>()
+    val hasSelectedTasks: LiveData<Boolean>
+        get() = _hasSelectedTasks
+
+    val selectedTasksQuantity: Int
+        get() = _taskList.value?.filter { it.isSelected }?.size ?: 1
 
     fun isTaskListEmpty() = taskList.value?.isEmpty() == true
 
-    fun delete(task: Task, toast: (Int) -> Unit) {
+    fun syncSelection(isSelected: Boolean, task: Task) {
         viewModelCall(
-            callToDo = { deleteTaskUseCase.execute(task) },
+            callToDo = { updateTaskUseCase.execute(task.copy(isSelected = isSelected)) },
+            onFinishCall = { refreshScreen() }
+        )
+    }
+
+    fun deleteSelectedTasks(toast: (Int) -> Unit) {
+        viewModelCall(
+            callToDo = { deleteSelectedTasksUseCase.execute() },
             onSuccess = { toast(R.string.task_successfully_deleted) },
-            onError = { toast(R.string.task_failure_on_delete) },
+            onError = { toast(R.string.tasks_failure_on_delete) },
             onFinishCall = { refreshScreen(toast) }
         )
     }
 
-    fun refreshScreen(onError: (Int) -> Unit) {
+    fun refreshScreen(onError: (Int) -> Unit = { }) {
         viewModelCall(
             callToDo = { getAllTasksUseCase.execute() },
-            onSuccess = { taskList.postValue(it) },
-            onError = { onError(R.string.task_failure_on_get_all) }
+            onSuccess = {
+                _taskList.postValue(it)
+                _hasSelectedTasks.value = it?.any { it.isSelected } == true
+                        },
+            onError = { onError(R.string.task_failure_on_get_all) },
         )
     }
 }
