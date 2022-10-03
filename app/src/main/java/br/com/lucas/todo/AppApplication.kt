d62f0d1fe948dbe439f87fa1ee2bad450056bb2b
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-03 16:58:30 -0300

    Adding timber plant

--- app/src/main/java/br/com/lucas/todo/AppApplication.kt
+++ app/src/main/java/br/com/lucas/todo/AppApplication.kt
@@ -2,6 +2,14 @@ package br.com.lucas.todo
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class AppApplication: Application()
+class AppApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
