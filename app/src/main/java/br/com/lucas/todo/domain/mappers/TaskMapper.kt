88dd2d6547b02e93b205b812a997054328724be6
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-03 16:58:53 -0300

    Creating a task mapper instead of use the ext fun

---
+++ app/src/main/java/br/com/lucas/todo/domain/mappers/TaskMapper.kt
@@ -0,0 +1,42 @@
+package br.com.lucas.todo.domain.mappers
+
+import br.com.lucas.todo.core.ext.*
+import br.com.lucas.todo.core.util.DateUtil
+import br.com.lucas.todo.data.db.entities.TaskEntity
+import br.com.lucas.todo.domain.model.Task
+import br.com.lucas.todo.domain.mappers.base.BaseMapper
+import java.util.*
+
+class TaskMapper: BaseMapper<TaskEntity, Task> {
+
+    override fun mapToDomainModel(entity: TaskEntity): Task {
+        return Task(
+            uuid = entity.uuid,
+            taskTitle = entity.taskTitle.orEmpty(),
+            taskDescription = entity.taskDescription.orEmpty(),
+            taskDate = DateUtil.formatLongToStringDate(entity.taskDate),
+            taskHour = entity.taskTime?.toHour().orEmpty(),
+            taskMinute = entity.taskTime?.toMinute().orEmpty(),
+            isSelected = entity.isSelected ?: false
+        )
+    }
+
+    override fun mapToEntity(domainModel: Task): TaskEntity {
+        return TaskEntity(
+            uuid = domainModel.uuid ?: UUID.randomUUID(),
+            taskTitle = domainModel.taskTitle,
+            taskDescription = domainModel.taskDescription,
+            taskDate = DateUtil.formatStringDateToLong(domainModel.taskDate),
+            taskTime = formatHoursAndMinutesToIntTime(domainModel.taskHour, domainModel.taskMinute),
+            isSelected = domainModel.isSelected
+        )
+    }
+
+    override fun mapToDomainModels(listEntity: List<TaskEntity>): List<Task> {
+        return listEntity.map { mapToDomainModel(it) }
+    }
+
+    override fun mapToEntities(listDomainModel: List<Task>): List<TaskEntity> {
+        return listDomainModel.map { mapToEntity(it) }
+    }
+}
