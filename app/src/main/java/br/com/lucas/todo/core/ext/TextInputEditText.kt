2ddb7dcdfde89286a59bd78c7801a565405b4190
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-03 17:07:04 -0300

    Fixing crash on try save new task with no time

--- app/src/main/java/br/com/lucas/todo/core/ext/TextInputEditText.kt
+++ app/src/main/java/br/com/lucas/todo/core/ext/TextInputEditText.kt
@@ -4,6 +4,14 @@ import com.google.android.material.textfield.TextInputEditText
 
 fun TextInputEditText.getStringText() = text?.trim().toString()
 
-fun TextInputEditText.getHour() = text?.split(':')?.get(0) ?: ""
+fun TextInputEditText.getHour() = try {
+    text?.split(':')?.get(0) ?: ""
+} catch (e: IndexOutOfBoundsException) {
+    ""
+}
 
-fun TextInputEditText.getMinute() = text?.split(':')?.get(1) ?: ""
+fun TextInputEditText.getMinute() = try {
+    text?.split(':')?.get(1) ?: ""
+} catch (e: IndexOutOfBoundsException) {
+    ""
+}
