8314f022012ba9f7387d268600c8c0dbc917d1f3
Lucas Moro <lucas.rmoro1@gmail.com>
2022-10-04 22:44:32 -0300

    Modifying the DiffUtilEquality interface

--- app/src/main/java/br/com/lucas/todo/presentation/base/adapter/DiffUtilEquality.kt
+++
@@ -1,8 +0,0 @@
-package br.com.lucas.todo.presentation.base.adapter
-
-interface DiffUtilEquality<T> {
-
-    fun areItemsTheSame(toCompare: T): Boolean
-    fun areContentsTheSame(toCompare: T): Boolean
-
-}
